ba71c0ed850d9d34db3a3b47ae2d58795d578016
zjccc1999 <82010245+zjccc1999@users.noreply.github.com>
2024-04-02 14:10:03 +0800

    Update 4-0 通用csv表格转格式.py

--- 电脑使用PY/4-0 通用csv表格转格式.py
+++ 电脑使用PY/4-0 通用csv表格转格式.py
@@ -3,6 +3,7 @@ from tkinter import filedialog
 import pandas as pd
 import os
 from datetime import datetime
+import chardet
 
 def select_file():
     file_path = filedialog.askopenfilename(title="选择CSV文件", filetypes=[("CSV files", "*.csv")])
@@ -10,7 +11,10 @@ def select_file():
         process_csv(file_path)
 
 def process_csv(file_path):
-    df = pd.read_csv(file_path, encoding='utf-8')
+    with open(file_path, 'rb') as f:
+        result = chardet.detect(f.read())
+    encoding = result['encoding']
+    df = pd.read_csv(file_path, encoding=encoding)
 
     if 'IP 地址' not in df.columns:
         print("CSV文件中不包含'IP 地址'列。")
@@ -40,9 +44,10 @@ root.title("选择CSV文件")
 select_button = tk.Button(root, text="选择CSV文件", command=select_file)
 select_button.pack(pady=20)
 
-# 添加处理result.csv文件按钮
+# 添加处理结果文件按钮
 def process_result_csv():
-    process_csv("result.csv")
+    file_path = "result.csv"  # 修改为实际的结果文件路径
+    process_csv(file_path)
 
 process_result_button = tk.Button(root, text="处理result.csv文件", command=process_result_csv)
 process_result_button.pack(pady=10)
