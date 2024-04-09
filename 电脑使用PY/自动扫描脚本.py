3375fc1de34f39be3c4bc3533b5bab74c2552cb0
zjccc1999 <82010245+zjccc1999@users.noreply.github.com>
2024-04-10 00:44:39 +0800

    Update 自动扫描脚本.py

--- 电脑使用PY/自动扫描脚本.py
+++ 电脑使用PY/自动扫描脚本.py
@@ -18,6 +18,8 @@ powershell_command_13 = "powershell.exe -Command \"ed iptest -a 3258 -e=false\""
 powershell_command_14 = "powershell.exe -Command \"ed iptest -a 8075 -e=false\""
 powershell_command_15 = "powershell.exe -Command \"ed iptest -a 9808 -e=false\""
 powershell_command_16 = "powershell.exe -Command \"ed iptest -a 16509 -e=false\""
+powershell_command_17 = "powershell.exe -Command \"ed iptest -a 209242 -e=false\""
+powershell_command_18 = "powershell.exe -Command \"ed iptest -a 13335 -e=false\""
 
 # 执行第一个 PowerShell 命令
 try:
@@ -26,8 +28,8 @@ try:
 except subprocess.CalledProcessError as e:
     print("第一个 PowerShell 程序执行失败：", e)
 
-# 等待 10 秒钟
-print("等待 10 秒钟...")
+# 等待 5 秒钟
+print("等待 5 秒钟...")
 time.sleep(10)
 
 # 执行第二个 PowerShell 命令
@@ -37,8 +39,8 @@ try:
 except subprocess.CalledProcessError as e:
     print("第二个 PowerShell 程序执行失败：", e)
 
-# 等待 10 秒钟
-print("等待 10 秒钟...")
+# 等待 5 秒钟
+print("等待 5 秒钟...")
 time.sleep(10)
 
 # 执行第三个 PowerShell 命令
@@ -48,8 +50,8 @@ try:
 except subprocess.CalledProcessError as e:
     print("第三个 PowerShell 程序执行失败：", e)
 
-# 等待 10 秒钟
-print("等待 10 秒钟...")
+# 等待 5 秒钟
+print("等待 5 秒钟...")
 time.sleep(10)
 
 # 执行第四个 PowerShell 命令
@@ -59,8 +61,8 @@ try:
 except subprocess.CalledProcessError as e:
     print("第四个 PowerShell 程序执行失败：", e)
 
-# 等待 10 秒钟
-print("等待 10 秒钟...")
+# 等待 5 秒钟
+print("等待 5 秒钟...")
 time.sleep(10)
 
 # 执行第五个 PowerShell 命令
@@ -70,8 +72,8 @@ try:
 except subprocess.CalledProcessError as e:
     print("第五个 PowerShell 程序执行失败：", e)
 
-# 等待 10 秒钟
-print("等待 10 秒钟...")
+# 等待 5 秒钟
+print("等待 5 秒钟...")
 time.sleep(10)
 
 # 执行第六个 PowerShell 命令
@@ -81,8 +83,8 @@ try:
 except subprocess.CalledProcessError as e:
     print("第六个 PowerShell 程序执行失败：", e)
 
-# 等待 10 秒钟
-print("等待 10 秒钟...")
+# 等待 5 秒钟
+print("等待 5 秒钟...")
 time.sleep(10)
 
 # 执行第七个 PowerShell 命令
@@ -92,8 +94,8 @@ try:
 except subprocess.CalledProcessError as e:
     print("第七个 PowerShell 程序执行失败：", e)
 
-# 等待 10 秒钟
-print("等待 10 秒钟...")
+# 等待 5 秒钟
+print("等待 5 秒钟...")
 time.sleep(10)
 
 # 执行第八个 PowerShell 命令
@@ -103,8 +105,8 @@ try:
 except subprocess.CalledProcessError as e:
     print("第八个 PowerShell 程序执行失败：", e)
 
-# 等待 10 秒钟
-print("等待 10 秒钟...")
+# 等待 5 秒钟
+print("等待 5 秒钟...")
 time.sleep(10)
 
 # 执行第九个 PowerShell 命令
@@ -114,8 +116,8 @@ try:
 except subprocess.CalledProcessError as e:
     print("第九个 PowerShell 程序执行失败：", e)
 
-# 等待 10 秒钟
-print("等待 10 秒钟...")
+# 等待 5 秒钟
+print("等待 5 秒钟...")
 time.sleep(10)
 
 # 执行第十个 PowerShell 命令
@@ -125,8 +127,8 @@ try:
 except subprocess.CalledProcessError as e:
     print("第十个 PowerShell 程序执行失败：", e)
 
-# 等待 10 秒钟
-print("等待 10 秒钟...")
+# 等待 5 秒钟
+print("等待 5 秒钟...")
 time.sleep(10)
 
 # 执行第十一个 PowerShell 命令
@@ -136,8 +138,8 @@ try:
 except subprocess.CalledProcessError as e:
     print("第十一个 PowerShell 程序执行失败：", e)
 
-# 等待 10 秒钟
-print("等待 10 秒钟...")
+# 等待 5 秒钟
+print("等待 5 秒钟...")
 time.sleep(10)
 
 # 执行第十二个 PowerShell 命令
@@ -147,8 +149,8 @@ try:
 except subprocess.CalledProcessError as e:
     print("第十二个 PowerShell 程序执行失败：", e)
 
-# 等待 10 秒钟
-print("等待 10 秒钟...")
+# 等待 5 秒钟
+print("等待 5 秒钟...")
 time.sleep(10)
 
 # 执行第十三个 PowerShell 命令
@@ -158,8 +160,8 @@ try:
 except subprocess.CalledProcessError as e:
     print("第十三个 PowerShell 程序执行失败：", e)
 
-# 等待 10 秒钟
-print("等待 10 秒钟...")
+# 等待 5 秒钟
+print("等待 5 秒钟...")
 time.sleep(10)
 
 # 执行第十四个 PowerShell 命令
@@ -169,8 +171,8 @@ try:
 except subprocess.CalledProcessError as e:
     print("第十四个 PowerShell 程序执行失败：", e)
 
-# 等待 10 秒钟
-print("等待 10 秒钟...")
+# 等待 5 秒钟
+print("等待 5 秒钟...")
 time.sleep(10)
 
 # 执行第十五个 PowerShell 命令
@@ -180,8 +182,8 @@ try:
 except subprocess.CalledProcessError as e:
     print("第十五个 PowerShell 程序执行失败：", e)
 
-# 等待 10 秒钟
-print("等待 10 秒钟...")
+# 等待 5 秒钟
+print("等待 5 秒钟...")
 time.sleep(10)
 
 # 执行第十六个 PowerShell 命令
@@ -191,25 +193,22 @@ try:
 except subprocess.CalledProcessError as e:
     print("第十六个 PowerShell 程序执行失败：", e)
 
+    # 执行第十七个 PowerShell 命令
+try:
+    subprocess.run(powershell_command_16, shell=True, check=True)
+    print("第十七个 PowerShell 程序执行成功！")
+except subprocess.CalledProcessError as e:
+    print("第十七个 PowerShell 程序执行失败：", e)
+
+# 执行第十八个 PowerShell 命令
+try:
+    subprocess.run(powershell_command_16, shell=True, check=True)
+    print("第十八个 PowerShell 程序执行成功！")
+except subprocess.CalledProcessError as e:
+    print("第十八个 PowerShell 程序执行失败：", e)
+
+
 import os
 import shutil
 
-# 获取桌面路径
-desktop_path = os.path.join(os.path.expanduser("~"), "Desktop")
-
-# 创建IP文件夹路径
-ip_folder_path = os.path.join(desktop_path, "IP")
-
-# 确保IP文件夹存在，如果不存在则创建
-if not os.path.exists(ip_folder_path):
-    os.makedirs(ip_folder_path)
-
-# 获取桌面上所有.csv文件的列表
-csv_files = [f for f in os.listdir(desktop_path) if f.endswith('.csv')]
-
-# 将所有.csv文件移动到IP文件夹内
-for csv_file in csv_files:
-    src_path = os.path.join(desktop_path, csv_file)
-    dest_path = os.path.join(ip_folder_path, csv_file)
-    shutil.move(src_path, dest_path)
 
